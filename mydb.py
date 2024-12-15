914e839b3ef97a58635000e8ce6035864eab064f
PlatonicM <mrunalchaudhari666@gmail.com>
2024-12-15 15:37:06 +0530

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'password123', 
+
+)
+
+# preaper a cursor object
+cursorObject = dataBase.cursor()
+
+# create a database
+cursorObject.execute("CREATE DATABASE raju")
+
+print("All Done!")
