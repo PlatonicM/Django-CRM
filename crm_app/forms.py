1bd7c489ce17d64121393a86df2e2df6bd333cee
PlatonicM <mrunalchaudhari666@gmail.com>
2024-12-16 14:01:23 +0530

    Recurtology CRM

--- crm_app/forms.py
+++ crm_app/forms.py
@@ -19,7 +19,7 @@ class SignUpForm(UserCreationForm):
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
-#from .models import Record
+from .models import Record
 
 
 # Creating sign up forms
@@ -52,10 +52,19 @@ class SignUpForm(UserCreationForm):
         self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
 
 
-    
+
+# create the Add Record form
+class AddRecordForm(forms.ModelForm):
+    first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "First Name", "class": "form-control"}), label="")
+    last_name  = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Last Name", "class": "form-control"}), label="")
+    email = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Email", "class": "form-control"}), label="")
+    phone = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Phone", "class": "form-control"}), label="")
+    address = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Address", "class": "form-control"}), label="")
+    city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "City", "class": "form-control"}), label="")
+    state = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "State", "class": "form-control"}), label="")
+    zipcode = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Zipcode", "class": "form-control"}), label="")
 
 
-
-# create the upload form
-#class UploadFileForm(forms.Form):
-    #file = forms.FileField()
+    class Meta:
+        model = Record
+        exclude = ("user",)
